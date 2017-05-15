4b6561fd320332fccab62a4762cbf772100e9c06
hjgoldman <haydengoldman@gmail.com>
2017-05-15 15:17:21 -0500

    Added route to get all data in db

--- Sources/App/Controllers/TasksViewController.swift
+++ Sources/App/Controllers/TasksViewController.swift
@@ -11,11 +11,50 @@ import Vapor
 import HTTP
 import VaporSQLite
 
+class Task : NodeRepresentable {
+    
+    var taskId :Int!
+    var title :String!
+    
+    func makeNode(context: Context) throws -> Node {
+        return try Node(node :["taskId":self.taskId,"title":self.title])
+    }
+}
+
+extension Task {
+    convenience init?(node :Node) {
+        self.init()
+        
+        guard let taskId  = node["taskId"]?.int,
+            let title = node["title"]?.string else {
+                return nil
+        }
+        
+        self.taskId = taskId
+        self.title = title
+    }
+}
+
 
 final class TasksViewController {
     
     func addRoutes (drop :Droplet) {
         
+        drop.get("tasks","all",handler :getAll)
+        
+    }
+    
+    func getAll(_req :Request) throws -> ResponseRepresentable {
+        
+        let result = try drop.database?.driver.raw("SELECT taskId, title from Tasks;")
+        
+        guard let nodes = result?.nodeArray else {
+            return try JSON(node :[])
+        }
+        
+        let tasks = nodes.flatMap(Task.init)
+        
+        return try JSON(node :tasks)
     }
     
     
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -6,4 +6,8 @@ let drop = Droplet()
 
 try drop.addProvider(VaporSQLite.Provider.self)
 
+let controller = TasksViewController()
+
+controller.addRoutes(drop: drop)
+
 drop.run()
